9f02b844adc77d04c633994ccdeaf3790faaae02
achs0 <info@roczniewski.de>
2015-03-09 21:51:33 +0100

    Java HelloWorld Application
    the simplest hello world application ever.

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,15 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output. Yes, i am serious!
+ 
+ 1. install the jdk from http://www.oracle.com/technetwork/java/javase/downloads/index.html
+ 2. set PATH Variable to your-path/jdk-version/bin
+ 3. compile this file with javac (will generate a class file)
+ 4. run programm with java -cp . HelloWorldApp
+ 
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
